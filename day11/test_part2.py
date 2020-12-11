3545e4dd16fc1725ded95c0f8331d451d900e7bc
Marius Gedminas <marius@gedmin.as>
2020-12-11 21:02:17 +0200

    2.3 seconds this time

---
+++ day11/test_part2.py
@@ -0,0 +1,91 @@
+from part2 import apply_rules
+
+
+seats = [s.splitlines() for s in """
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+
+#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##
+
+#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#
+
+#.L#.##.L#
+#L#####.LL
+L.#.#..#..
+##L#.##.##
+#.##.#L.##
+#.#####.#L
+..#.#.....
+LLL####LL#
+#.L#####.L
+#.L####.L#
+
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##LL.LL.L#
+L.LL.LL.L#
+#.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLL#.L
+#.L#LL#.L#
+
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.#L.L#
+#.L####.LL
+..#.#.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+
+#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#
+""".strip().split("\n\n")]
+
+
+def test_apply_rules():
+    assert apply_rules(seats[0]) == seats[1]
+    assert apply_rules(seats[1]) == seats[2]
+    assert apply_rules(seats[2]) == seats[3]
+    assert apply_rules(seats[3]) == seats[4]
+    assert apply_rules(seats[4]) == seats[5]
+    assert apply_rules(seats[5]) == seats[6]
+    assert apply_rules(seats[6]) == seats[6]
