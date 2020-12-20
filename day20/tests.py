37f39675105fba4d48f9a590b83831506b5f9b4c
Marius Gedminas <marius@gedmin.as>
2020-12-20 13:58:33 +0200

    Part 2.
    This was hard, not because difficult, but because I'm not a smart
    person.

--- day20/tests.py
+++ day20/tests.py
@@ -1,15 +1,41 @@
-from part1 import tile_edges
+from part2 import Tile, flip, rotate, tile_edges
 
 
 def test_tile_edges():
-    tile = [
+    tile = Tile([
         "123",
         "894",
         "765",
-    ]
-    assert set(tile_edges(tile)) == {
+    ])
+    assert tile_edges(tile) == [
         "123",
         "345",
         "567",
         "781",
-    }
+    ]
+
+
+def test_rotate():
+    tile = Tile([
+        "123",
+        "894",
+        "765",
+    ])
+    assert rotate(tile) == [
+        "345",
+        "296",
+        "187",
+    ]
+
+
+def test_flip():
+    tile = Tile([
+        "123",
+        "894",
+        "765",
+    ])
+    assert flip(tile) == [
+        "765",
+        "894",
+        "123",
+    ]
